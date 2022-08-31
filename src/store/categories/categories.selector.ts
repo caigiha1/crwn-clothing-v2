c8e71440363ad558c039f77814227aae25089118
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-08-31 16:09:57 +0700

    update categories store to typescript

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -1,23 +1,26 @@
 import { createSelector } from 'reselect';
 
-const selectCategoriesReducer = (state) => state.categories;
+import { CategoriesState } from './categories.reducer';
+import { CategoryMap } from './categories.types';
+
+const selectCategoriesReducer = (state): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoriesReducer],
   (categoriesSlice) => categoriesSlice.categories
 );
 
-export const selectCategoriesIsLoading = createSelector(
-  [selectCategoriesReducer],
-  (categoriesSlice) => categoriesSlice.isLoading
-);
-
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
+  (categories): CategoryMap =>
     categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
 );
